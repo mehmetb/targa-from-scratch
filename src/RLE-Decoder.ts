282c5ee2df25bc78b9bbe5541ef254a2e9051a85
Mehmet Baker <mehmet@mehmetbaker.dev>
2023-11-02 13:48:46 +0300

    further RLE decode optimization
    Signed-off-by: Mehmet Baker <mehmet@mehmetbaker.dev>

--- src/RLE-Decoder.ts
+++ src/RLE-Decoder.ts
@@ -1,80 +1,64 @@
-class RLEDecoder {
-  private readArray: Uint8Array;
-  private writeArray: Uint8Array;
-  private pixelSize: number;
-
-  private readCursor = 0;
-  private writeCursor = 0;
-
-  constructor(
-    readArray: Uint8Array,
-    writeArray: Uint8Array,
-    pixelSize: number
-  ) {
-    this.readArray = readArray;
-    this.writeArray = writeArray;
-    this.pixelSize = pixelSize;
-  }
-
-  private read(): number {
-    return this.readArray[this.readCursor++];
-  }
-
-  private write(value: number) {
-    this.writeArray[this.writeCursor++] = value;
-  }
-
-  private readNextPixel(): number[] {
-    const arr: number[] = [];
-
-    for (let i = 0; i < this.pixelSize; ++i) {
-      arr.push(this.read());
-    }
-
-    return arr;
-  }
-
-  private writePixel(pixel: number[]) {
-    for (const byte of pixel) {
-      this.write(byte);
-    }
-  }
-
-  decode(): void {
-    console.time('decode loop');
-    const readArrayLength = this.readArray.byteLength;
-
-    for (let i = 0; i < readArrayLength; ++i) {
-      const packet = this.read();
-
-      // RLE packet
-      if (packet >= 128) {
-        const repetition = packet - 128;
-        const nextPixel = this.readNextPixel();
-
-        for (let i = 0; i <= repetition; ++i) {
-          this.writePixel(nextPixel);
-        }
-      } else {
-        // raw packet
-        const repetition = packet;
-
-        for (let i = 0; i <= repetition; ++i) {
-          const nextPixel = this.readNextPixel();
-          this.writePixel(nextPixel);
-        }
-      }
-    }
-
-    console.timeEnd('decode loop');
-  }
-}
-
 export function decodeRunLengthEncoding(
   readArray: Uint8Array,
   writeArray: Uint8Array,
-  pixelSize: number
+  pixelSize: number,
 ): void {
-  const decoder = new RLEDecoder(readArray, writeArray, pixelSize);
-  decoder.decode();
+  console.time('decode loop');
+  const readArrayLength = readArray.byteLength;
+  let readCursor = 0;
+  let writeCursor = 0;
+  let byte1;
+  let byte2;
+  let byte3;
+  let byte4;
+
+  for (let i = 0; i < readArrayLength; ++i) {
+    const packet = readArray[readCursor++];
+
+    // RLE packet
+    if (packet >= 128) {
+      const repetition = packet - 128;
+      byte1 = readArray[readCursor++];
+
+      if (pixelSize > 2) {
+        byte2 = readArray[readCursor++];
+        byte3 = readArray[readCursor++];
+      }
+
+      if (pixelSize > 3) {
+        byte4 = readArray[readCursor++];
+      }
+
+      for (let i = 0; i <= repetition; ++i) {
+        writeArray[writeCursor++] = byte1;
+
+        if (pixelSize > 2) {
+          writeArray[writeCursor++] = byte2;
+          writeArray[writeCursor++] = byte3;
+        }
+
+        if (pixelSize > 3) {
+          writeArray[writeCursor++] = byte4;
+        }
+      }
+    } else {
+      // raw packet
+      const repetition = packet;
+
+      for (let i = 0; i <= repetition; ++i) {
+        writeArray[writeCursor++] = readArray[readCursor++];
+
+        if (pixelSize > 2) {
+          writeArray[writeCursor++] = readArray[readCursor++];
+          writeArray[writeCursor++] = readArray[readCursor++];
+        }
+
+        if (pixelSize > 3) {
+          writeArray[writeCursor++] = readArray[readCursor++];
+        }
+      }
+    }
+  }
+
+  console.timeEnd('decode loop');
 }
