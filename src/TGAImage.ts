e4a0e0a873a1769b97d8221cc4a9355be6af09cf
Mehmet Baker <mehmet@mehmetbaker.dev>
2023-11-04 15:10:54 +0300

    Optimized RLE Color Mapped and Uncompressed Grayscale
    Signed-off-by: Mehmet Baker <mehmet@mehmetbaker.dev>

--- src/TGAImage.ts
+++ src/TGAImage.ts
@@ -36,6 +36,29 @@ export default class TGAImage {
     }
   }
 
+  private drawUncompressedGrayscale(imageData: ImageData) {
+    console.time('uncompressed loop');
+    const { imageHeight, imageWidth, topToBottom } = this.stats;
+    const { data } = imageData;
+    const { imageDataBytes } = this;
+    data.fill(255);
+
+    for (let y = 0; y < imageHeight; ++y) {
+      for (let x = 0; x < imageWidth; ++x) {
+        const canvasOffset = topToBottom
+          ? y * imageWidth * 4 + x * 4
+          : (imageHeight - y - 1) * imageWidth * 4 + x * 4;
+
+        const byteOffset = x + y * imageWidth;
+        data[canvasOffset] = imageDataBytes[byteOffset];
+        data[canvasOffset + 1] = imageDataBytes[byteOffset];
+        data[canvasOffset + 2] = imageDataBytes[byteOffset];
+      }
+    }
+
+    console.timeEnd('uncompressed loop');
+  }
+
   private drawUncompressed(imageData: ImageData) {
     console.time('uncompressed loop');
     const { imageHeight, imageWidth, pixelSize, topToBottom } = this.stats;
@@ -52,14 +75,6 @@ export default class TGAImage {
         data[canvasOffset + 3] = 255;
 
         switch (pixelSize) {
-          case 1: {
-            const byteOffset = x + y * imageWidth;
-            data[canvasOffset] = imageDataBytes[byteOffset];
-            data[canvasOffset + 1] = imageDataBytes[byteOffset];
-            data[canvasOffset + 2] = imageDataBytes[byteOffset];
-            break;
-          }
-
           case 3: {
             const byteOffset = y * imageWidth * 3 + x * 3;
             data[canvasOffset] = imageDataBytes[byteOffset + 2];
@@ -370,7 +385,140 @@ export default class TGAImage {
     console.timeEnd('color mapped loop');
   }
 
-  draw(canvas: HTMLCanvasElement) {
+  private drawRunLengthEncodedColorMapped(imageData: ImageData) {
+    console.time('run length encoded color mapped loop');
+    const { imageHeight, imageWidth, pixelSize, topToBottom, imageIdentificationFieldLength, colorMapOrigin, imageDataFieldOffset, colorMapPixelSize } = this.stats;
+    const { data } = imageData;
+    const { imageDataBytes, bytes, dataView } = this;
+    const readArrayLength = imageDataBytes.length;
+    const padding = 18 + imageIdentificationFieldLength + colorMapOrigin;
+    let readCursor = 0;
+    let x = 0;
+    let y = 0;
+    let byte1 = 0;
+    let byte2 = 0;
+    let byte3 = 0;
+    let byte4 = 0;
+    let colorMapEntryOffset: number = 0;
+
+    for (let i = 0; i < readArrayLength; ++i) {
+      const packet = imageDataBytes[readCursor++];
+
+      // RLE packet
+      if (packet >= 128) {
+        if (pixelSize === 1) {
+          colorMapEntryOffset = padding + colorMapPixelSize * imageDataBytes[readCursor++];
+        } else {
+          colorMapEntryOffset = padding + colorMapPixelSize * dataView.getUint16(imageDataFieldOffset + readCursor, true);
+          readCursor += 2;
+        }
+
+        const repetition = packet - 128;
+        byte1 = bytes[colorMapEntryOffset];
+
+        if (colorMapPixelSize > 2) {
+          byte2 = bytes[colorMapEntryOffset + 1];
+          byte3 = bytes[colorMapEntryOffset + 2];
+        }
+
+        if (colorMapPixelSize > 3) {
+          byte4 = bytes[colorMapEntryOffset + 3];
+        }
+
+        for (let i = 0; i <= repetition; ++i) {
+          const canvasOffset = topToBottom
+            ? y * imageWidth * 4 + x * 4
+            : (imageHeight - y - 1) * imageWidth * 4 + x * 4;
+
+          data[canvasOffset + 3] = 255;
+
+          switch (colorMapPixelSize) {
+            case 1: {
+              data[canvasOffset] = byte1;
+              data[canvasOffset + 1] = byte1;
+              data[canvasOffset + 2] = byte1;
+              break;
+            }
+
+            case 3: {
+              data[canvasOffset] = byte3;
+              data[canvasOffset + 1] = byte2;
+              data[canvasOffset + 2] = byte1;
+              break;
+            }
+
+            case 4: {
+              data[canvasOffset] = byte3;
+              data[canvasOffset + 1] = byte2;
+              data[canvasOffset + 2] = byte1;
+              data[canvasOffset + 3] = byte4;
+              break;
+            }
+          }
+
+          if (x === imageWidth - 1) {
+            x = 0;
+            y += 1;
+          } else {
+            x += 1;
+          }
+        }
+      } else {
+        // raw packet
+        const repetition = packet;
+
+        for (let i = 0; i <= repetition; ++i) {
+          const canvasOffset = topToBottom
+            ? y * imageWidth * 4 + x * 4
+            : (imageHeight - y - 1) * imageWidth * 4 + x * 4;
+
+          if (pixelSize === 1) {
+            colorMapEntryOffset = padding + colorMapPixelSize * imageDataBytes[readCursor++];
+          } else {
+            colorMapEntryOffset = padding + colorMapPixelSize * dataView.getUint16(imageDataFieldOffset + readCursor, true);
+            readCursor += 2;
+          }
+
+          data[canvasOffset + 3] = 255;
+
+          switch (colorMapPixelSize) {
+            case 1: {
+              data[canvasOffset] = bytes[colorMapEntryOffset];
+              data[canvasOffset + 1] = bytes[colorMapEntryOffset];
+              data[canvasOffset + 2] = bytes[colorMapEntryOffset];
+              break;
+            }
+
+            case 3: {
+              data[canvasOffset] = bytes[colorMapEntryOffset + 2];
+              data[canvasOffset + 1] = bytes[colorMapEntryOffset + 1];
+              data[canvasOffset + 2] = bytes[colorMapEntryOffset];
+              break;
+            }
+
+            case 4: {
+              data[canvasOffset] = bytes[colorMapEntryOffset + 2];
+              data[canvasOffset + 1] = bytes[colorMapEntryOffset + 1];
+              data[canvasOffset + 2] = bytes[colorMapEntryOffset];
+              data[canvasOffset + 3] = bytes[colorMapEntryOffset + 3];
+              break;
+            }
+          }
+
+          if (x === imageWidth - 1) {
+            x = 0;
+            y += 1;
+          } else {
+            x += 1;
+          }
+        }
+      }
+    }
+
+    console.timeEnd('run length encoded color mapped loop');
+  }
+
+  async draw(canvas: HTMLCanvasElement) {
     console.time('draw');
     const context = canvas.getContext('2d');
 
@@ -387,12 +535,20 @@ export default class TGAImage {
     const begin = performance.now();
 
     if (this.stats.rleEncoded) {
-      this.drawRunLengthEncoded(imageData);
+      if (this.stats.imageType === ImageType.RUN_LENGTH_ENCODED_COLOR_MAPPED) {
+        this.drawRunLengthEncodedColorMapped(imageData);
+      } else {
+        this.drawRunLengthEncoded(imageData);
+      }
     } else {
       if (this.stats.imageType === ImageType.COLOR_MAPPED) {
         this.drawColorMapped(imageData);
       } else {
-        this.drawUncompressed(imageData);
+        if (this.stats.pixelSize === 1) {
+          this.drawUncompressedGrayscale(imageData);
+        } else {
+          this.drawUncompressed(imageData);
+        }
       }
     }
 
