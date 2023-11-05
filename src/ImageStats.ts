5e78a973b1dcc6334f11e005e86fa8b5265c592a
Mehmet Baker <mehmet@mehmetbaker.dev>
2023-11-05 11:40:05 +0300

    Read extension area for TGA v2
    Signed-off-by: Mehmet Baker <mehmet@mehmetbaker.dev>

--- src/ImageStats.ts
+++ src/ImageStats.ts
@@ -1,4 +1,4 @@
-import { ImageType, ImageDescriptorFields } from './types';
+import { ImageType, ImageDescriptorFields, AttributesType } from './types';
 
 export class ImageStats {
   #arrayBuffer: ArrayBuffer;
@@ -14,16 +14,32 @@ export class ImageStats {
   imageWidth: number;
   imageHeight: number;
   pixelSize: number;
+  pixelSizeRaw: number;
   imageDescriptor: number;
   imageIdentificationFieldLength: number;
   imageDataFieldOffset: number;
   colorMapOrigin: number;
   colorMapLength: number;
   colorMapPixelSize: number;
-  extensionOffset: number;
+  extensionOffset: number = 0;
   version: 1 | 2;
   topToBottom: boolean;
   duration: number = 0;
+
+  authorName: string|undefined;
+  authorComments: string|undefined;
+  dateTimeStamp: Date|undefined;
+  jobId: string|undefined;
+  jobTime: string|undefined;
+  softwareId: string|undefined;
+  softwareVersion: string|undefined;
+  keyColor: { red: number, green: number, blue: number, alpha: number }|undefined;
+  aspectRatio: string|undefined;
+  gammaValue: string|undefined;
+  colorCorrectionOffset: number = 0;
+  postageStampOffset: number = 0;
+  scanLineOffset: number;
+  attributesType: AttributesType|undefined;
 
   get arrayBuffer() {
     return this.#arrayBuffer;
@@ -48,12 +64,17 @@ export class ImageStats {
     this.imageWidth = this.dataView.getUint16(12, true);
     this.imageHeight = this.dataView.getUint16(14, true);
     this.pixelSize = this.bytes[16] / 8;
+    this.pixelSizeRaw = this.bytes[16];
     this.imageDescriptor = this.bytes[17];
     this.imageDataFieldOffset = this.getImageDataFieldOffset();
     this.detectVersion();
 
     if (this.version === 2) {
       this.extensionOffset = this.dataView.getUint32(this.dataView.byteLength - 26, true);
+
+      if (this.extensionOffset !== 0) {
+        this.readExtension();
+      }
     }
 
     this.topToBottom = this.isTopToBottom();
@@ -107,5 +128,93 @@ export class ImageStats {
     }
 
     return this.arrayBuffer.byteLength;
+  }
+
+  readExtension() {
+    const extensionSize = this.dataView.getUint16(this.extensionOffset, true);
+
+    if (extensionSize !== 495) {
+      console.warn('Not a valid TGA extension');
+      return;
+    }
+
+    const EO = this.extensionOffset;
+    const textDecoder = new TextDecoder();
+
+    const readString = (startOffset: number, endOffset: number): string => {
+      const buffer = this.bytes.subarray(startOffset, endOffset);
+      return textDecoder.decode(buffer);
+    }
+
+    const readShorts = (startOffset: number, numberOfShortsToRead: number): number[] => {
+      const shorts: number[] = [];
+      const endOffset = startOffset + numberOfShortsToRead * 2;
+
+      for (let offset = startOffset; offset < endOffset; offset += 2) {
+        shorts.push(this.dataView.getUint16(offset, true));
+      }
+      
+      return shorts;
+    }
+
+    this.authorName = readString(EO + 1, EO + 42);
+    this.authorComments = readString(EO + 42, EO + 366);
+    this.jobId = readString(EO + 379, EO + 419);
+    this.softwareId = readString(EO + 426, EO + 466);
+    this.softwareVersion = readString(EO + 467, EO + 469);
+
+    const [month, day, year, hour, minute, second] = readShorts(EO + 367, 6);
+
+    if (year !== 0) {
+      this.dateTimeStamp = new Date(year, month - 1, day, hour, minute, second);
+    }
+
+
+    this.jobTime = '0';
+
+    const jobTimeStrParts: string[] = [];
+    const jobTimeObject = {
+      hour: this.dataView.getUint16(EO + 420, true),
+      minute: this.dataView.getUint16(EO + 422, true),
+      second: this.dataView.getUint16(EO + 424, true),
+    }
+
+    for (const [key, value] of Object.entries(jobTimeObject)) {
+      if (value > 1) {
+        jobTimeStrParts.push(`${value} ${key}s`);
+      } else if (value > 0) {
+        jobTimeStrParts.push(`${value} ${key}`);
+      }
+    }
+
+    this.jobTime = jobTimeStrParts.join(' ');
+
+    const blue = this.bytes[EO + 470];
+    const green = this.bytes[EO + 471];
+    const red = this.bytes[EO + 472];
+    const alpha = this.bytes[EO + 473];
+    this.keyColor = { red, green, blue, alpha};
+
+    const [aspectRatioNumerator, aspectRatioDenominator] = readShorts(EO + 474, 2);
+    
+    if (aspectRatioDenominator !== 0) {
+      this.aspectRatio = `${aspectRatioNumerator}/${aspectRatioDenominator}`;
+    }
+
+    const [gammaNumerator, gammaDenominator] = readShorts(EO + 478, 2);
+    
+    if (gammaDenominator !== 0) {
+      this.gammaValue = `${gammaNumerator}/${gammaDenominator}`;
+    }
+
+    this.colorCorrectionOffset = this.dataView.getUint32(EO + 482, true);
+    this.postageStampOffset = this.dataView.getUint32(EO + 486, true);
+    this.scanLineOffset = this.dataView.getUint32(EO + 490, true);
+
+    const attr = this.dataView.getUint8(EO + 494);
+
+    if (AttributesType[attr]) {
+      this.attributesType = attr;
+    }
   }
 }
