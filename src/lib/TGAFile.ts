9646ab86808ccda9c42d56ec183f0fa6f307acb6
Mehmet Baker <mehmet@mehmetbaker.dev>
2024-09-28 15:55:07 +0300

    Added license headers to each source file
    Signed-off-by: Mehmet Baker <mehmet@mehmetbaker.dev>

--- src/lib/TGAFile.ts
+++ src/lib/TGAFile.ts
@@ -1,3 +1,23 @@
+/** 
+ * @license
+ * Copyright 2024 Mehmet Baker
+ *
+ * This file is part of tga-for-web.
+ *
+ * tga-for-web is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * tga-for-web is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with tga-for-web. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import ImageFileInfo from './ImageFileInfo';
 
 export default class TGAFile {
